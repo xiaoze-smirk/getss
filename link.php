cdf2338b5767c699378be9ab2c11c5cb32bcf6e5
ovolqs <35006001+ovolqs@users.noreply.github.com>
2018-06-25 18:43:49 +0800

    Update link.php

--- link.php
+++ link.php
@@ -1,13 +1,18 @@
 <?php
 require_once 'functions.php';
+
+$is_dingyue = isset($_GET['dingyue']) && $_GET['dingyue'] == '1';
 $filename = 'abc.txt';
 $file = getFileContent($filename);
-$link = getLink($file);
+$link = getSSLink($file);
 $text = "";
-foreach ($link as $item){
-    $text .="$item\n";
+
+foreach ($link as $item) {
+
+    $text .= genNode($item, $is_dingyue) . "\n";
+
 }
-if ($_GET['dingyue'] == '1') {
+if ($is_dingyue) {
     echo base64_encode($text);
 } else {
     echo $text;
